71bdc5388b2bf1550a41562354be395c6ec1473e
ABHIN M <abhin18m@gmail.com>
2025-07-14 21:02:28 +0530

    joi validation

---
+++ identity-service/src/controllers/identity-controller.js
@@ -0,0 +1,10 @@
+const logger = require("../utils/logger");
+
+const registerUser = async (req, res) => {
+    logger.info('Register Endpoint hit...')
+    try {
+        
+    } catch (e) {
+
+    }
+}
